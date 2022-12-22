9b9bbc144e384993ee68bf23bd1402702f867599
yasinarafat39 <yasinarafat2557@gmail.com>
2022-12-22 06:07:20 +0600

    Faq updated

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Faq = () => {
     return (
         <div>
-            <h2>this is FAQ page</h2>
+            <h2 className='text-5xl font-semibold text-center my-128'>Coming........</h2>
         </div>
     );
 };
