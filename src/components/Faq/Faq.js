622d34c5dfd1a974f51eaaae4655c04b0dc97bc1
yasinarafat39 <yasinarafat2557@gmail.com>
2022-12-22 06:12:31 +0600

    FAQ text margin updated

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Faq = () => {
     return (
         <div>
-            <h2 className='text-5xl font-semibold text-center my-50'>Coming........</h2>
+            <h2 className='text-5xl font-semibold text-center my-56'>Coming........</h2>
         </div>
     );
 };
